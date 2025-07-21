d2963583c1610942d62ee000145117651bbb8425
Dhanashri Chile <chiledhanashri@gmail.com>
2025-07-22 00:02:06 +0530

    name change

--- ConsoleApp1/NewClass1.cs
+++ ConsoleApp1/NewClass1.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
 {
     public class NewClass1
     {
-        public void method1()
+        public void methodName()
         {
             Console.WriteLine("Hello..........");
             Console.WriteLine("New Line Test");
